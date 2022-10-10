3c5b841141da95b67168fa2bfa91a6d3a126364f
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-10 18:42:49 -0300

    XML home added

---
+++ app/src/main/java/com/blogappdemo/data/model/Post.kt
@@ -0,0 +1,11 @@
+package com.blogappdemo.data.model
+
+
+import java.sql.Timestamp
+
+data class Post(
+    val profile_picture: String = "",
+    val profile_name: String = "",
+    val post_timestamp: Timestamp? = null,
+    val post_image: String = "",
+)
---
+++ app/src/main/java/com/blogappdemo/ui/home/HomeScreenFragment.kt
@@ -0,0 +1,15 @@
+package com.blogappdemo.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.blogappdemo.R
+
+class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
