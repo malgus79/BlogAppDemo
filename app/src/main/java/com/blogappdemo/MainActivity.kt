fcaf54293d3739908cbfe7084c5c7881e5e0215f
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-10 13:35:11 -0300

    Upload photo to storage

--- app/src/main/java/com/blogappdemo/MainActivity.kt
+++ app/src/main/java/com/blogappdemo/MainActivity.kt
@@ -6,17 +6,19 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var imageView: ImageView
-    private val REQUEST_IMAGE_CAPTURE = 1
     private lateinit var resultLauncher: ActivityResultLauncher<Intent?>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,11 +28,12 @@ class MainActivity : AppCompatActivity() {
         resultLauncher = registerForActivityResult(
             ActivityResultContracts
                 .StartActivityForResult()
-        ){
+        ) {
             if (it.resultCode == Activity.RESULT_OK) {
                 val data: Intent? = it.data
                 val imageBitmap = data?.extras?.get("data") as Bitmap
                 imageView.setImageBitmap(imageBitmap)
+                uploadPicture(imageBitmap)
             }
         }
 
@@ -49,6 +52,35 @@ class MainActivity : AppCompatActivity() {
             resultLauncher.launch(takePictureIntent)
         } catch (e: ActivityNotFoundException) {
             Toast.makeText(this, "No se encontro app para tomar la foto", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    //subir foto al storage
+    private fun uploadPicture(bitmap: Bitmap) {
+        //crear una referencia en el storage
+        val storageRef = FirebaseStorage.getInstance().reference
+        val imageRef = storageRef.child("image.jpg")
+        val baos = ByteArrayOutputStream()
+        //comprimir la foto
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        //transformar la imagen en un ByteArray
+        val data = baos.toByteArray()
+
+        val uploadTask = imageRef.putBytes(data)
+
+        uploadTask.continueWithTask { task ->
+            if (!task.isSuccessful) {
+                task.exception?.let { exception ->
+                    throw exception
+                }
+            }
+            imageRef.downloadUrl
+        //consumir el task
+        }.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val downloadUri = task.result.toString()
+                Log.d("Storage", "uploadPicture: $downloadUri")
+            }
         }
     }
 }
