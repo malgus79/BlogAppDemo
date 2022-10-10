2ddd96898e4340d10216a5e8df16fdad14b742df
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-10 12:04:00 -0300

    addOnSuccessListener added

--- app/src/main/java/com/blogappdemo/MainActivity.kt
+++ app/src/main/java/com/blogappdemo/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.blogappdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+
+        //Consultar informacion
+        db.collection("ciudades").document("NY").get().addOnSuccessListener { document ->
+            document.let {
+                val ciudad = document.toObject(Ciudad::class.java)
+                Log.d("Firebase", "Color: ${ciudad?.color}")
+                Log.d("Firebase", "Population: ${ciudad?.population}")
+                Log.d("Firebase", "Postal code: ${ciudad?.cp}")
+            }
+        }.addOnFailureListener { error ->
+            Log.d("FirebaseError", error.toString())
+        }
+
+        //Ingresar informacion
+        db.collection("ciudades").document("LA").set(Ciudad(300000, "Red")).addOnSuccessListener {
+            Log.d("Firebase", "Se guardo la ciudad correctamente")
+        }.addOnFailureListener { error ->
+            Log.d("FirebaseError", error.toString())
+        }
     }
 }
+
+data class Ciudad(
+    val population: Int = 0,
+    val color: String = "",
+    val cp: Int = 0
+)
