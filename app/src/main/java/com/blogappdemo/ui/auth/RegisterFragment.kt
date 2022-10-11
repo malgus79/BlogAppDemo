e1a559c043d18ab90cdbd0f718c0f02479abeb01
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-11 13:12:38 -0300

    Validating registry data

--- app/src/main/java/com/blogappdemo/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/blogappdemo/ui/auth/RegisterFragment.kt
@@ -1,10 +1,9 @@
 package com.blogappdemo.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.blogappdemo.R
 import com.blogappdemo.databinding.FragmentRegisterBinding
 
@@ -15,5 +14,46 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentRegisterBinding.bind(view)
+        signUp()
+
+    }
+
+    private fun signUp() {
+        binding.btnSignup.setOnClickListener {
+
+            val username = binding.editTextUsername.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+            val confirmPassword = binding.editTextConfirmPassword.text.toString().trim()
+            val email = binding.editTextEmail.text.toString().trim()
+
+
+            if (password != confirmPassword) {
+                binding.editTextConfirmPassword.error = "Password does not match"
+                binding.editTextPassword.error = "Password does not match"
+                return@setOnClickListener
+            }
+
+            if (username.isEmpty()) {
+                binding.editTextUsername.error = "Username is empty"
+                return@setOnClickListener
+            }
+
+            if (email.isEmpty()) {
+                binding.editTextEmail.error = "Email is empty"
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                binding.editTextPassword.error = "Password is empty"
+                return@setOnClickListener
+            }
+
+            if (confirmPassword.isEmpty()) {
+                binding.editTextConfirmPassword.error = "Confirm Password is empty"
+                return@setOnClickListener
+            }
+
+            Log.d("signUpData", "data: $username $password $confirmPassword $email ")
+        }
     }
 }
