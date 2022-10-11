b2318427fd1747f4c9ea14ad683b943e3246989e
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-11 18:01:58 -0300

    Take photo

--- app/src/main/java/com/blogappdemo/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/blogappdemo/ui/camera/CameraFragment.kt
@@ -1,19 +1,48 @@
 package com.blogappdemo.ui.camera
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.blogappdemo.R
 import com.blogappdemo.databinding.FragmentCameraBinding
 
 class CameraFragment : Fragment(R.layout.fragment_camera) {
 
     private lateinit var binding: FragmentCameraBinding
+    private val REQUEST_IMAGE_CAPTURE = 2
+    private var bitmap: Bitmap? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCameraBinding.bind(view)
+
+        //abrir la camara
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(requireContext(), "No se encontro app para abir la camara", Toast.LENGTH_SHORT).show()
+        }
     }
+
+    //callback al volver de tomar la foto
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.imgAddPhoto.setImageBitmap(imageBitmap)
+            bitmap = imageBitmap
+        }
+    }
+
 }
