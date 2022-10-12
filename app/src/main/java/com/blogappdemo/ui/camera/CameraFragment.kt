c26c6b9b29fcbab4999eab978d103198e7c7bffc
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-11 21:24:03 -0300

    Fixed decrecated onActivityResult in fragmentCamera

--- app/src/main/java/com/blogappdemo/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/blogappdemo/ui/camera/CameraFragment.kt
@@ -29,29 +29,21 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCameraBinding.bind(view)
 
-        val btnUploadPicture = binding.btnUploadPhoto
-        btnUploadPicture.setOnClickListener {
-            dispatchTakePictureIntent()
-        }
-
         //solucion al onActivityResult @deprecated
         resultLauncher = registerForActivityResult(
             ActivityResultContracts
                 .StartActivityForResult()
         ){
             if (it.resultCode == Activity.RESULT_OK) {
-                val data: Intent? = it.data
-                val imageBitmap = data?.extras?.get("data") as Bitmap
-                binding.imgAddPhoto.setImageBitmap(imageBitmap)
+                val imageBitmap = it.data?.extras?.get("data") as Bitmap
+                binding.postImage.setImageBitmap(imageBitmap)
                 bitmap = imageBitmap
             }
         }
-    }
 
-    //abrir la camara
-    private fun dispatchTakePictureIntent() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        //abrir la camara
         try {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             resultLauncher.launch(takePictureIntent)
         } catch (e: ActivityNotFoundException) {
             Toast.makeText(requireContext(), "No se encontro app para abir la camara", Toast.LENGTH_SHORT).show()
