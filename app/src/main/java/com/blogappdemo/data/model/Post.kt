4c02fa8389f020f3bddfa2ac444e68e105df4ef5
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-12 18:25:16 -0300

    Refactor Data Post

--- app/src/main/java/com/blogappdemo/data/model/Post.kt
+++ app/src/main/java/com/blogappdemo/data/model/Post.kt
@@ -1,14 +1,24 @@
 package com.blogappdemo.data.model
 
+import com.google.firebase.firestore.Exclude
 import com.google.firebase.firestore.ServerTimestamp
 import java.util.*
 
 data class Post(
-    val profile_picture: String = "",
-    val profile_name: String = "",
-    @ServerTimestamp
+    @Exclude @JvmField  //para excluir de firebase
+    val id: String = "",
+    @ServerTimestamp  //transformar la info que viene de firebase que lo mapea a date
     var created_at: Date? = null,
     val post_image: String = "",
     val post_description: String = "",
-    val uid: String = "",
+    val poster: Poster? = null,
+    val likes: Long = 0,
+    @Exclude @JvmField //para excluir de firebase
+    val liked: Boolean = false,
+)
+
+data class Poster(
+    val username: String? = "",
+    val uid: String? = null,
+    val profile_picture: String = "",
 )
