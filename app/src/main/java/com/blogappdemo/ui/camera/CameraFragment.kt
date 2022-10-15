725ce8741fb385dc3a8f3ee655563c06b1e3ebea
Gustavo Maldonado <gamcba7982@gmail.com>
2022-10-15 14:10:46 -0300

    Fixed bugs navigation after uploading

--- app/src/main/java/com/blogappdemo/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/blogappdemo/ui/camera/CameraFragment.kt
@@ -8,7 +8,6 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -16,6 +15,7 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.blogappdemo.R
 import com.blogappdemo.core.Result
+import com.blogappdemo.core.hideKeyboard
 import com.blogappdemo.data.remote.camera.CameraDataSource
 import com.blogappdemo.databinding.FragmentCameraBinding
 import com.blogappdemo.domain.camera.CameraRepoImpl
@@ -26,7 +26,6 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
 
     private lateinit var binding: FragmentCameraBinding
     private var bitmap: Bitmap? = null
-    private lateinit var resultLauncher: ActivityResultLauncher<Intent?>
     private val viewModel by viewModels<CameraViewModel> {
         CameraViewModelFactory(CameraRepoImpl(
             CameraDataSource()))
@@ -36,60 +35,50 @@ class CameraFragment : Fragment(R.layout.fragment_camera) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCameraBinding.bind(view)
 
-        binding.btnTakePhoto.setOnClickListener {
-            openCamera()
-        }
-
         //solucion al onActivityResult @deprecated
-        resultLauncher = registerForActivityResult(
-            ActivityResultContracts
-                .StartActivityForResult()
-        ) {
-            if (it.resultCode == Activity.RESULT_OK) {
-                val imageBitmap = it.data?.extras?.get("data") as Bitmap
-                binding.postImage.setImageBitmap(imageBitmap)
-                bitmap = imageBitmap
+        val resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    val imageBitmap = it.data?.extras?.get("data") as Bitmap
+                    binding.postImage.setImageBitmap(imageBitmap)
+                    bitmap = imageBitmap
+                }
             }
-        }
 
-        binding.btnUploadPhoto.setOnClickListener {
-            bitmap?.let {
-                viewModel.uploadPhoto(it, binding.etxtDescription.text.toString().trim()).observe(viewLifecycleOwner, Observer { result ->
-                     when (result) {
-                         is Result.Loading -> {
-                             Toast.makeText(requireContext(),"Uploading photo...", Toast.LENGTH_SHORT).show()
-                         }
-                         is Result.Success -> {
-                             findNavController().navigate(R.id.action_cameraFragment_to_homeScreenFragment)
-                         }
-                         is Result.Failure -> {
-                             Toast.makeText(requireContext(),"Error ${result.exception}", Toast.LENGTH_SHORT).show()
-                         }
-                     }
-                })
-            }
-        }
-    }
-
-    //abrir la camara
-    private fun openCamera() {
+        //abrir la camara
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         try {
-            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             resultLauncher.launch(takePictureIntent)
         } catch (e: ActivityNotFoundException) {
             Toast.makeText(requireContext(),
                 "No se encontro app para abir la camara",
                 Toast.LENGTH_SHORT).show()
         }
-    }
 
-//    //deprecado
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-//            val imageBitmap = data?.extras?.get("data") as Bitmap
-//            binding.imgAddPhoto.setImageBitmap(imageBitmap)
-//            bitmap = imageBitmap
-//        }
-//    }
+        binding.btnUploadPhoto.setOnClickListener {
+            bitmap?.let {
+                viewModel.uploadPhoto(it, binding.etxtDescription.text.toString().trim())
+                    .observe(viewLifecycleOwner, Observer { result ->
+                        when (result) {
+                            is Result.Loading -> {
+                                Toast.makeText(requireContext(),
+                                    "Uploading photo...",
+                                    Toast.LENGTH_SHORT).show()
+                            }
+                            is Result.Success -> {
+                                //findNavController().navigate(R.id.action_cameraFragment_to_homeScreenFragment)
+                                findNavController().popBackStack()
+                                binding.etxtDescription.setText("")
+                            }
+                            is Result.Failure -> {
+                                Toast.makeText(requireContext(),
+                                    "Error ${result.exception}",
+                                    Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    })
+            }
+            it.hideKeyboard()
+        }
+    }
 }
